aed8007f2a79a8214b3abcc6002ff6ae27661f9d
qrLogin <30745823+qrLogin@users.noreply.github.com>
2017-08-05 15:46:37 +0300

    1
    1

--- acp/acp_qrlogin_info.php
+++ acp/acp_qrlogin_info.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
- *
- * qrlogin extension for phpBB.
- *
- * @copyright (c) 2017 qrlogin <http://qrlogin.info>
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* qrlogin extension for phpBB.
+*
+* @copyright (c) 2017 qrlogin <http://qrlogin.info>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
 
 namespace qrlogin\qrlogin\acp;
 
 class acp_qrlogin_info
 {
 
-	function module()
-	{
+    function module()
+    {
 		return array(
 			'filename' => '\qrlogin\qrlogin\acp\acp_qrlogin_module',
 			'title'	=> 'ACP_QRLOGIN',
 			'modes'	=> array(
-				'settings' => array(
-					'title' => 'SETTINGS',
+                'settings' => array(
+                    'title' => 'SETTINGS',
 					'auth'  => 'ext_qrlogin/qrlogin && acl_a_board',
 					'cat'   => array('ACP_QRLOGIN')
 				),
